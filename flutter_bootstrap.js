e5f7f752e196ed4ee25198f5ce195a06f1618c4c
Vishal <vp078596@gmail.com>
2024-09-05 21:25:25 +0530

    disabled hash based routing to support deep links

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1577859169"
+    serviceWorkerVersion: "1230269853"
   }
 });
